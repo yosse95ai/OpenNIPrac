6adf44a621c2ddc1198cf490ad7f8fca1dfe7095
yosse95ai <karateyosi@gmail.com>
2020-08-21 22:41:44 +0900

    8

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 # OpenNIPrac
 OpenNIの練習
 結果・コード
-## prac1 res
+
+## PRAC1
 カラーカメラ画像の表示
 
 ![prac1](https://github.com/yosse95ai/OpenNIPrac/blob/images/images/ret1.png)
-## prac2 res
+## PRAC2
 Depthカメラの距離データを使ってみる
 
 ![prac1](https://github.com/yosse95ai/OpenNIPrac/blob/images/images/ret2.png)
 
-## prac3 res
+## PRAC3
 ストリームデータの情報を使ってみる
 
 [JUMP RESULT >>](https://raw.githubusercontent.com/yosse95ai/OpenNIPrac/images/ret/ret3.txt)
 
-## prac4 res
+## PRAC4
 出力をミラー化する
 
 ミラー
@@ -26,18 +27,23 @@ Depthカメラの距離データを使ってみる
 
 ![nonMirror](https://github.com/yosse95ai/OpenNIPrac/blob/images/images/nonMirror.png)
 
-## prac5 res
+## PRAC5
 クロッピングについて
 
 よくわからなかった...
 
-## prac6 res
+## PRAC6
 IR画像を出力
 
 ![IRStream](https://github.com/yosse95ai/OpenNIPrac/blob/images/images/ret6.png)
 
-## prac7 res
+## PRAC7
 ストリームデータの保存
 
 .oniファイルとして映像記録を保存する
+
+## PRAC8
+ストリームデータの再生
+
+![streamPlay](https://github.com/yosse95ai/OpenNIPrac/blob/images/images/ret8.png)
 
---
+++ code/OpenNiPrac8.cpp
@@ -0,0 +1,160 @@
+﻿#include <iostream>
+#include <stdexcept>
+#include <OpenNI.h>
+#include <opencv2/opencv.hpp>
+
+class DepthSensor
+{
+public:
+    void initialize()
+    {
+        // デバイスを取得
+        openni::Status ret = device.open("recoder.oni");
+        if (ret != openni::STATUS_OK) {
+            throw std::runtime_error("openni::Device::open() failed.");
+        }
+
+        // カラーストリームを有効にする
+        colorStream.create(device, openni::SENSOR_IR);
+        colorStream.start();
+
+        // Depthストリームを有効にする
+        depthStream.create(device, openni::SENSOR_DEPTH);
+        depthStream.start();
+
+        // ストリームデータを記録する
+        //recoder.create("recoder.oni");
+        //recoder.attach(colorStream);
+        //recoder.attach(depthStream);
+        //recoder.start();
+    }
+
+    // フレームの更新処理
+    void update()
+    {
+        openni::VideoFrameRef colorFrame;
+        openni::VideoFrameRef depthFrame;
+
+        // 更新されたフレームの取得
+        colorStream.readFrame(&colorFrame);
+        depthStream.readFrame(&depthFrame);
+
+        // フレームデータを表示できる形に変換する
+        colorImage = showColorStream(colorFrame);
+        depthImage = showDepthStream(depthFrame);
+
+        // フレームデータを表示する
+        cv::imshow("Color Stream", colorImage);
+        cv::imshow("Depth Stream", depthImage);
+    }
+
+private:
+    void changeResolutioin(openni::VideoStream& stream)
+    {
+        openni::VideoMode mode = stream.getVideoMode();
+        mode.setResolution(640, 480);
+        mode.setFps(30);
+        stream.setVideoMode(mode);
+    }
+
+    // カラーストリームを表示できる形に変換する
+    cv::Mat showColorStream(const openni::VideoFrameRef& colorFrame)
+    {
+        cv::Mat colorImage;
+
+        // Color Stream
+        if (colorFrame.getVideoMode().getPixelFormat() == openni::PIXEL_FORMAT_RGB888) {
+            // OpeCVの形にする
+            cv::Mat colorImage = cv::Mat(colorFrame.getHeight(), colorFrame.getWidth(),
+                CV_8UC3, (unsigned char*)colorFrame.getData());
+
+            // BGR 2 RGB
+            cv::cvtColor(colorImage, colorImage, cv::COLOR_RGB2BGR);
+        }
+        // Xtion IR Stream
+        else if (colorFrame.getVideoMode().getPixelFormat() == openni::PIXEL_FORMAT_GRAY16) {
+            // XtionのIRのフォーマットは16ビットグレースケール
+            // 実際は255諧調らしく、CV_8Uに落とさないとえない
+            colorImage = cv::Mat(colorFrame.getHeight(), colorFrame.getWidth(),
+                CV_16UC1, (unsigned char*)colorFrame.getData());
+            colorImage.convertTo(colorImage, CV_8U);
+        }
+        // Kinect for Windows IR Stream
+        else {
+            // KinectのIRのフォーマットは8ビットグレースケール
+            // Kinect SDKは16ビットグレイスケール
+            colorImage = cv::Mat(colorFrame.getHeight(), colorFrame.getWidth(),
+                CV_8UC1, (unsigned char*)colorFrame.getData());
+        }
+
+        return colorImage;
+    }
+
+    // Depthストリームを表示できる形に変換する
+    cv::Mat showDepthStream(const openni::VideoFrameRef& depthFrame)
+    {
+        // 距離データを画像化する(16bits)
+        cv::Mat depthImage = cv::Mat(depthFrame.getHeight(),
+            depthFrame.getWidth(),
+            CV_16UC1, ((unsigned short*)depthFrame.getData()));
+
+        // 0~10000mmまでのデータを0~255に正規化
+        depthImage.convertTo(depthImage, CV_8U, 255.0 / 10000);
+
+        // 中心点の距離を表示
+        showCenterDistance(depthImage, depthFrame);
+
+        return depthImage;
+    }
+
+    void showCenterDistance(cv::Mat& depthImage, const openni::VideoFrameRef& depthFrame)
+    {
+        openni::VideoMode videoMode = depthStream.getVideoMode();
+
+        int centerX = videoMode.getResolutionX() / 2;
+        int centerY = videoMode.getResolutionY() / 2;
+        int centerIndex = (centerY * videoMode.getResolutionX()) + centerX;
+
+        unsigned short* depth = (unsigned short*)depthFrame.getData();
+
+        std::stringstream ss;
+        ss << "Center Point :" << depth[centerIndex];
+        cv::putText(depthImage, ss.str(), cv::Point(0, 50),
+            cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255));
+    }
+
+    //
+    openni::Device device;              // 使用するデバイス
+    openni::VideoStream colorStream;    // カラーストリーム
+    openni::VideoStream depthStream;    //  Depthストリーム
+    openni::Recorder recoder;
+    cv::Mat colorImage;                 // 表示用データ
+    cv::Mat depthImage;                 // Depth表示用データ
+
+};
+
+int main(int argc, const char* argv[])
+{
+    try {
+        // OpenNIを初期化
+        openni::OpenNI::initialize();
+
+        // センサーを初期化する
+        DepthSensor sensor;
+        sensor.initialize();
+
+        // メインループ
+        while (1) {
+            sensor.update();
+            int key = cv::waitKey(10);
+            if (key == 'q') {
+                break;
+            }
+        }
+    }
+    catch (std::exception&) {
+        std::cout << openni::OpenNI::getExtendedError() << std::endl;
+    }
+
+    return 0;
+}
